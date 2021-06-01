c96950a84ba80f6fb8aeef3f0b3a9a91ff7214e5
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-06-01 06:40:07 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int FibonacciRecursion(int n){
+    if(n <= 1){
+        return n;
+    }
+    else{
+        return FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2);
+    }
+}
+
+int FibonacciIterative(int n){
+    if(n <= 1){
+        return n;
+    }
+    else{
+        int pN;
+        int ppN;
+        int cN = 1;
+        for(int i = 1; i < n; i++){
+            ppN = pN;
+            pN = cN;
+            cN = ppN + pN;
+        }
+        return cN;
+    }
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    cout << FibonacciRecursion(11) << endl;
+    cout << FibonacciIterative(11) << endl;
 }
