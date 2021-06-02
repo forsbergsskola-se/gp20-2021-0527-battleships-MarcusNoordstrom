21af80e2aa4510c146ebdc6775f5a10b52070f89
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-06-02 15:54:22 +0200

    Added Battleships

---
+++ assignments/Battleships/GameField.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Bulten on 2021-06-02.
+//
+
+#include "GameField.h"
---
+++ assignments/Battleships/GameField.h
@@ -0,0 +1,15 @@
+//
+// Created by Bulten on 2021-06-02.
+//
+
+#ifndef BATTLESHIPS_GAMEFIELD_H
+#define BATTLESHIPS_GAMEFIELD_H
+
+
+class GameField {
+public:
+    int pOneField[10][10];
+    int pTwoField[10][10];
+};
+
+#endif //BATTLESHIPS_GAMEFIELD_H
