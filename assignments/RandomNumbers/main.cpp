9d0bdb32d78a5ceaaf2063a2dc3997c023c32f55
Bulteze <48334398+Bulteze@users.noreply.github.com>
2021-06-04 11:21:04 +0200

    Cleanup + check - Done

--- assignments/RandomNumbers/main.cpp
+++ assignments/RandomNumbers/main.cpp
@@ -7,9 +7,9 @@ int GetRandomNumber(int start, int end){
 }
 
 //Checks for occurrences in numbers for nCheck.
-int CheckNo(const int numbers[1000],int nCheck){
+int CheckNo(const int numbers[10000],int nCheck){
     int count = 0;
-    for(int i = 0; i < 1000; i++){
+    for(int i = 0; i < 10000; i++){
         if(numbers[i] == nCheck){
             count += 1;
         }
@@ -18,7 +18,7 @@ int CheckNo(const int numbers[1000],int nCheck){
 }
 
 //Our numbers
-int numbers [1000];
+int numbers [10000];
 
 //Generate all numbers randomly.
 void GenerateNumbers(){
@@ -28,13 +28,13 @@ void GenerateNumbers(){
     }
 }
 
-//Check all numbers occurrences and their total percentage of 1000
+//Check all numbers occurrences and their total percentage of 10000
 void CheckAllNumbers(){
-    std::cout << "Using RAND + random seed. 1000 different numbers" << std::endl;
+    std::cout << "Using RAND + random seed. 10000 different numbers" << std::endl;
     std::cout << "||||||||||||||||||||||||||||||||||||||||||||||||" << std::endl;
     for(int i = 1; i < 21; i++){
         int occ = CheckNo(numbers, i);
-        float percent = ((float) occ / 1000) * 100;
+        float percent = ((float) occ / 10000) * 100;
         std::cout << "Occurrences of: " << i << " - " << occ << " times" << std::endl;
         std::cout << "Total % of " << i << ": " << percent << '%' << std::endl;
         std::cout << "--------------------------" << std::endl;
@@ -45,11 +45,10 @@ void CheckAllNumbers(){
 int main() {
     //Sets a random seed.
     srand(time(nullptr));
-    //Generates 1000 numbers.
+    //Generates 10000 numbers.
     GenerateNumbers();
     //Checks occurrences.
     CheckAllNumbers();
 
     return 0;
 }
-
